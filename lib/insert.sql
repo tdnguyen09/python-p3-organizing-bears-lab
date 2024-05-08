3d7c2dd1e3589cf93a7a787095caa203c913e03f
Thuy Duong Nguyen <thuyduong.nguyen.091096@gmail.com>
2024-05-09 00:33:46 +1000

    complete lab

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
+('Mr. Chocolate', 3, 'M', 'brown', 'calm', 1),
+('Rowdy', 5, 'M', 'brown', 'calm', 1),
+('Tabitha', 3, 'F', 'black', 'calm', 1),
+('Sergeant Brown', 2, 'M', 'brown', 'calm', 1),
+('Melissa', 6, 'F', 'brown', 'angry', 0),
+('Grinch', 8, 'F', 'white', 'calm', 1),
+('Wendy', 10, 'M', 'white', 'sleppy', 0),
+(NULL, NULL, 'M', 'unknown', 'unknown', 1);
